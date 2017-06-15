2c1c8f94b96d72ed2a318414bdfadf2fea001641
soo <carlsagan96@gmail.com>
2017-06-16 04:35:47 +0900

    bububu

--- src/event_based_server/Constants.java
+++ src/event_based_server/Constants.java
@@ -6,12 +6,12 @@ package event_based_server;
 class Constants {
     static final int PORT = 8080;
     static final String HOST = "localhost";
-    static final int MAIN_BUFFER_SIZE = 4096;
+    static final int MAIN_BUFFER_SIZE = 300;
     static final int PERIODIC_SELECT = 300;
     static final int THREAD_POOL_SIZE = Runtime.getRuntime().availableProcessors();
     static final int CACHE_TIME_TO_LIVE = 200;
     static final int CACHE_TIMER_INTERVAL = 500;
     static final int CACHE_MAX_ITEMS = 100;
     static final long CACHE_BYTE_SIZE = 100 * 1024 * 1024; // 100M
-    static final int REQUEST_TIMEOUT_MILLIS = 30 * 1000000;
+    static final int REQUEST_TIMEOUT_MILLIS = 30 * 1000;
 }
